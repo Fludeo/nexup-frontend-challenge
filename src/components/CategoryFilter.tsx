6f729e9b884c47fd9245a40d64b7ab8f7913ef9c
Leandro Medina <medinaleandron@gmail.com>
2026-01-08 10:07:35 -0300

    Add basic category filters functionality and styles

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ProductCategory } from '../models/ProductCategory';
 
-interface CategoryFilterProps {}
+interface CategoryFilterProps {
+  onNewFilters: (filters: ProductCategory[]) => void;
+}
 
-export const CategoryFilter: React.FC<CategoryFilterProps> = () => {
-  return <div />;
+export const CategoryFilter: React.FC<CategoryFilterProps> = ({
+  onNewFilters,
+}) => {
+  const [categories] = useState<ProductCategory[]>(
+    Object.values(ProductCategory),
+  );
+  const [appliedFilters, setAppliedFilters] = useState<Set<ProductCategory>>(
+    new Set(),
+  );
+
+  const onFilterChange = (filter: ProductCategory) => {
+    const newFilters = new Set(appliedFilters);
+    if (newFilters.has(filter)) newFilters.delete(filter);
+    else newFilters.add(filter);
+    setAppliedFilters(newFilters);
+    onNewFilters([...newFilters]);
+  };
+  return (
+    <div>
+      {categories.length > 0 && (
+        <div>
+          <h2>Categories</h2>
+
+          {categories.map((category) => (
+            <button
+              type="button"
+              key={category}
+              style={{
+                backgroundColor: appliedFilters.has(category)
+                  ? 'blue'
+                  : 'white',
+                color: appliedFilters.has(category) ? 'white' : 'black',
+              }}
+              onClick={() => onFilterChange(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
